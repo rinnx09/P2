f81317d68d4c67696bb99f08af321015992a270c
taruc <taruc@B14>
2017-10-31 11:00:57 +0800

    first-branch commit

--- src/Practical2.java
+++ src/Practical2.java
@@ -14,7 +14,7 @@ public class Practical2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("First-branch");
         // TODO code application logic here
     }
     
